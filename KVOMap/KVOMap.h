092ede9cda0457be40c87cae05ebb95bb09e6a33
Alex Gray <alex@mrgray.com>
2013-10-01 02:48:46 -0400

    Proper Mac OS X static lib built target.

---
+++ KVOMap/KVOMap.h
@@ -0,0 +1,10 @@
+
+
+
+#import <KVOMap/DCKeyValueObjectMapping.h>
+#import <KVOMap/DCGenericConverter.h>
+#import <KVOMap/DCDynamicAttribute.h>
+#import <KVOMap/DCReferenceKeyParser.h>
+#import <KVOMap/DCPropertyFinder.h>
+#import <KVOMap/DCAttributeSetter.h>
+#import <KVOMap/DCDictionaryRearranger.h>
